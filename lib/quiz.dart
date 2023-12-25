62f64a674c132ddf84dcd37a45fc991007eabba6
NicoProsser <prossernico@gmail.com>
2023-12-25 21:59:09 +0100

    Setup and add button for switching

---
+++ lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 131, 183, 226),
+                  Color.fromARGB(255, 1, 75, 145)
+                ],
+              ),
+            ),
+            child: activeScreen),
+      ),
+    );
+  }
+}
